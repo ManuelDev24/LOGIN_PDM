a6f1a400ceb710a35041420d7ec65186d8a8d4d9
ManuelDev24 <msoliveymedrano@gmail.com>
2024-08-10 04:25:12 -0400

    Add project files.

---
+++ LOGIN_PDM/LOGIN_PDM/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LOGIN_PDM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Método para manejar el evento Clicked del ImageButton
+        private void btnOk_Clicked(object sender, EventArgs e)
+        {
+            // Lógica para el botón "OK"
+        }
+    }
+}
